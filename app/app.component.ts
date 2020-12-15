10b1e2052fe4c5666587b2ff28e18d42a9ee8fc8
rahiyansafz <rahiyansafin@gmail.com>
2020-12-15 15:58:34 +0600

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,37 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent {
   myStyle: object = {};
-    myParams: object = {};
- 
-    ngOnInit() {
-        this.myStyle = {
-            'width': '100%',
-            'height': '100%',
-            'top': 0,
-            'left': 0,
-            'right': 0,
-            'bottom': 0,
-        };
- 
-    this.myParams = {
-            particles: {
-                number: {
-                    value: 200,
-                },
-                color: {
-                    value: '#ff0000'
-                },
-                shape: {
-                    type: 'triangle',
-                },
-        }
+  myParams: object = {};
+
+  ngOnInit() {
+    this.myStyle = {
+      width: "100%",
+      height: "100%",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0
     };
-    }
-    
+
+    this.myParams = {
+      particles: {
+        number: {
+          value: 200
+        },
+        color: {
+          value: "#ff0000"
+        },
+        shape: {
+          type: "triangle"
+        }
+      }
+    };
+  }
 }
